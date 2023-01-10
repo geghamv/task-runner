42f0f87c9195927493205b82bf8b4a0d964bffdf
Gegham Vardanyan <gegham@Geghams-MacBook-Pro.local>
2023-01-11 03:19:41 +0400

    added environment variable DATABASE URL

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask
 from db import db
 import logging
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/gegham"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql://localhost/gegham')
 logging.basicConfig(level=logging.DEBUG)
